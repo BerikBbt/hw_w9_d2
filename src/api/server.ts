be73e1fc0e03cea0c84bbfe31b38c44d97c170d3
Berik <bbaibaturov@gmail.com>
2023-12-07 10:38:41 -0500

    homework_wk9_d3

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,4 +1,4 @@
-let accessToken = ""
+let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTkyMDEzNSwianRpIjoiNjU3ZWJlOWYtYTQyYi00YzJlLWIxZDctNjdiNzZjMmViMTlhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImJlcmlra3ltIiwibmJmIjoxNzAxOTIwMTM1LCJleHAiOjE3MzM0NTYxMzV9.AwHETTGakhHYjepZMSTg_jfpR-UJFefAW1os5GUSa_s"
 let userId = localStorage.getItem('uuid') 
 
 
@@ -9,7 +9,7 @@ export const serverCalls = {
 
     getShop: async () => {
        
-        const response = await fetch(`https://rangers134-shopas.onrender.com/api/shop`, {
+        const response = await fetch(`https://flask-final-homework.onrender.com/api/shop`, {
             method: 'GET',
             headers: {
                 'Content-Type' : 'application/json',
